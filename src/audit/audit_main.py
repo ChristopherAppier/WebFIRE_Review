f5eb10394ece74f27af9f936cb8e5a731f0452f6
Chris Appier <appier.christopher@epa.gov>
2026-07-08 22:59:58 -0500

    Flushing out placeholders

---
+++ src/audit/audit_main.py
@@ -0,0 +1,14 @@
+def main(config, paths):
+
+    return
+
+if __name__ == "__main__":
+    from common import utilities
+
+    # Load configuration
+    config = utilities.load_config()
+
+    # Builds the paths for the data directories
+    paths = utilities.build_paths(config)
+
+    main(config, paths)
--- src/python_review/python_review_main.py
+++ src/python_review/python_review_main.py
@@ -1,1 +1,14 @@
-from common import utilities
+def main(config, paths):
+
+    return
+
+if __name__ == "__main__":
+    from common import utilities
+
+    # Load configuration
+    config = utilities.load_config()
+
+    # Builds the paths for the data directories
+    paths = utilities.build_paths(config)
+
+    main(config, paths)
