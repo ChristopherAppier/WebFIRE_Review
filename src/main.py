0b98e0b0cb2aa3986d8ea3144b44b5a5e67fca32
Chris Appier <appier.christopher@epa.gov>
2026-09-15 17:06:10 -0500

    Folder simplification and cleanup based on current functionality

--- src/main.py
+++ src/main.py
@@ -1,29 +1,25 @@
-from ai_review.pipeline import main as ai_review
 from audit.pipeline import main as audit_reviews
 from common import startup
-from python_review.pipeline import main as python_review
 from retrieve.pipeline import main as retrieve_documents
+from review.pipeline import main as review_documents
 from summarize.pipeline import main as summarize_reviews
 
 
 def main():
 
-    # Setting up logging and loading configuration options and paths from settings.yml
+    # Sets up logging and loads configuration options and file paths from settings.yml
     config, paths = startup.initialize_project()
 
-    # Runs the document retrieval process
+    # Retrieves documents from WebFIRE
     retrieve_documents(config, paths)
 
-    # Runs the LLM review process
-    ai_review(config, paths)
+    # Reviews the documents using a lightweight LLM
+    review_documents(config, paths)
 
-    # Runs the python review process
-    python_review(config, paths)
-
-    # Runs the audit process
+    # Audits flagged documents and their lightweight LLM review using a heavyweight LLM
     audit_reviews(config, paths)
 
-    # Runs the summary report building process
+    # Builds a summary report based on the LLM reviews
     summarize_reviews(config, paths)
 
 
